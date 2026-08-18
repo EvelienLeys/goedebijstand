8423f2afc606b5aafe5b9cd85378a6009a162404
Goede Bijstand <2096@diomb.be>
2026-08-18 13:40:41 +0200

    Update agenda.js
    filmopnames sept

--- js/agenda.js
+++ js/agenda.js
@@ -81,6 +81,8 @@ const FIXED = [
   { datum:'2026-08-15', tijd:'11:00', type:'viering', tk:'ev_olvhemelvaart' },
   { datum:'2026-09-12', tijd:'19:00', type:'viering', tk:'ev_evensong' },
   { datum:'2026-09-13', tijd:'13:30', type:'cleanup', tk:'ev_koesterkoffie' },
+  { datum:'2026-09-18', tijd:'00:00', type:'filmopname', tk:'ev_filmopname' },
+  { datum:'2026-09-19', tijd:'00:00', type:'filmopname', tk:'ev_filmopname' },
 ];
 
 const ZONDAG_UITZONDERINGEN = {
