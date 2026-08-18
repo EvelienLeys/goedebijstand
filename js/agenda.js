6fd7462f0183bc0e1cade7ae3db9cc3e3fdd1450
Goede Bijstand <2096@diomb.be>
2026-08-18 13:41:34 +0200

    Update agenda.js
    Huwelijk Bram en Evelien

--- js/agenda.js
+++ js/agenda.js
@@ -83,6 +83,7 @@ const FIXED = [
   { datum:'2026-09-13', tijd:'13:30', type:'cleanup', tk:'ev_koesterkoffie' },
   { datum:'2026-09-18', tijd:'00:00', type:'filmopname', tk:'ev_filmopname' },
   { datum:'2026-09-19', tijd:'00:00', type:'filmopname', tk:'ev_filmopname' },
+  { datum:'2026-09-19', tijd:'11:00', type:'huwelijk', tk:'ev_huwelijk_leys_carlier' },
 ];
 
 const ZONDAG_UITZONDERINGEN = {
